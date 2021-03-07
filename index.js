ce71f5f52c73b364a53d85f71fae01f6c37965bc
Gustavo Mafra <lgmafra@gmail.com>
2021-03-07 03:00:54 -0300

    trying update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (request, response) => {
-  return response.json({ message: "Hello world" });
+  return response.json({ message: "Hello world Gustavo" });
 });
 
 app.listen(3000);
